57701cadb8bd1377dccc33a619fb31c9720006c2
Seiya Hayashi <shayashi@hitomedia.jp>
2020-07-23 19:53:23 +0900

    メソッドリファクタリング

--- lambda_function.rb
+++ lambda_function.rb
@@ -10,30 +10,8 @@ class Scraping
   class Freitag
     class << self
       def run
-        bags_dom = open(base_url_of_bags) do |f|
-          charset = f.charset
-          f.read
-        end
-
-        bags_page = Nokogiri::HTML.parse(bags_dom)
-        bags = bags_page.search(".neo-unikat-model").map do |bag|
-          name_with_type = bag.search(".content-wrapper h3").text
-          type = name_with_type.split(" ")[0]
-          name = name_with_type.split(" ")[1]
-
-          price = bag.search(".content-wrapper .field-commerce-price").text.strip
-          list_url = bag.search(".sector-model-unikat-pictures-json").attribute("data-model-url").value
-          image_url = bag.search(".sector-model-unikat-pictures-json img").attribute("src").value
-          # code = list_url の末尾のコード
-
-          {
-            type: type,
-            name: name,
-            price: price,
-            list_url: list_url,
-            image_url: image_url,
-          }
-        end
+        bags = bags_from_html
+        JSON.parse(bags)
       end
 
       private
@@ -41,12 +19,47 @@ class Scraping
         def base_url_of_bags
           "https://www.freitag.ch/en/shop/bags"
         end
+
+        def bags_from_html
+          bags_dom = open(base_url_of_bags) do |f|
+            charset = f.charset
+            f.read
+          end
+  
+          bags_page = Nokogiri::HTML.parse(bags_dom)
+          bags = bags_page.search(".neo-unikat-model").map do |bag|
+            name_with_type = bag.search(".content-wrapper h3").text
+            type = name_with_type.split(" ")[0]
+            name = name_with_type.split(" ")[1]
+  
+            price = bag.search(".content-wrapper .field-commerce-price").text.strip
+            list_url = bag.search(".sector-model-unikat-pictures-json").attribute("data-model-url").value
+            image_url = bag.search(".sector-model-unikat-pictures-json img").attribute("src").value
+            # code = list_url の末尾のコード
+  
+            Bag.new({ type: type,
+                      name: name,
+                      price: price,
+                      list_url: list_url,
+                      image_url: image_url })
+          end
+        end
     end
   end
 end
 
-def lambda_handler
+class Bag
+  attr_accessor :type, :name, :price, :list_url, :image_url
 
+  def initialize(params)
+    @type = params[:type]
+    @name = params[:name]
+    @price = params[:price]
+    @list_url = params[:list_url]
+    @image_url = params[:image_url]
+  end
 end
 
-Scraping::Freitag.run
+def lambda_handler
+  Scraping::Freitag.run
+end
